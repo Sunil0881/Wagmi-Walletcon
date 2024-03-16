3c23c91877288135351de945a7c10c5cefd8795d
Sunil_0881 <sunil12162003@gmail.com>
2024-03-16 19:15:07 +0530

    fc

---
+++ dapp/src/App.js
@@ -0,0 +1,17 @@
+import Button from "./components/Button";
+import CustomButton from "./components/CustomButton";
+
+function App() {
+  return (
+    <div className="App">
+      <div>
+          <Button />
+      </div>
+      <div>
+           <CustomButton />
+      </div>
+    </div>
+  );
+}
+
+export default App;
